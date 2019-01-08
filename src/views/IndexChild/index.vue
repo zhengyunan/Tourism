6f79a41d8e9c22c737519d210018472f3a03e46c
zhengyunan <2802493252@qq.com>
2019-01-09 02:21:42 +0800

    中间部分基本完成 差边框去除

--- src/views/IndexChild/index.vue
+++ src/views/IndexChild/index.vue
@@ -14,7 +14,7 @@
                 <div class="city">
                     <router-link to="/">
                         <p>沭阳县</p>
-                        
+                         
                             <img src="../../assets/images/button_triangle.png" alt="">
                     
                     </router-link>
@@ -40,11 +40,42 @@
                 </div>
             </div>
         </header>
-        <div class="category">
-           <div class="category_hotal">
-                <div class="hotal_left">12</div>
-                <div class="hotal_middle">34</div>
-                <div class="hotal_right">56</div>
+        <div class="big_category">
+           <div class="category_hotal small_category">
+                <div class="hotal_left total_left"><p>酒店</p></div>
+                <div class="hotal_middle middle_coumn">
+                    <div class="hotal_middle_top category_s">特惠酒店</div>
+                    <div class="hotal_middle_bottom category_s">客栈</div>
+                </div>
+                <div class="hotal_right middle_coumn">
+                    <div class="hotal_middle_top category_s">海外酒店</div>
+                    <div class="hotal_middle_bottom category_s">民宿</div>
+                </div>
+           </div>
+           <div class="category_fligth small_category">
+                <div class="fligth_left total_left"><p>机票</p></div>
+                <div class="fligth_middle middle_coumn">
+                    <div class="fligth_middle_top category_s">低价机票</div>
+                    <div class="fligth_middle_bottom category_s">火车票</div>
+                </div>
+                <div class="fligth_right middle_coumn">
+                    <div class="fligth_middle_top category_s">汽车·船票</div>
+                    <div class="fligth_middle_bottom category_s">专车·自驾</div>
+                </div>
+           </div>
+           <div class="category_holiday small_category">
+                <div class="holiday_left total_left"><p>度假</p></div>
+                <div class="holiday_middle middle_coumn">
+                    <div class="holiday_middle_top category_s">景点·门票</div>
+                    <div class="holiday_middle_bottom category_s">自由行</div>
+                </div>
+                <div class="holiday_right middle_coumn">
+                    <div class="holiday_middle_top category_s">旅行团购</div>
+                    <div class="holiday_middle_bottom category_s">周边·短途</div>
+                </div>
+           </div>
+           <div class="category_money">
+               
            </div>
         </div>
         
