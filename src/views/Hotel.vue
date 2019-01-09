2aae678be2d0baee32bd610cf60e9beab4d22cb3
zhengyunan <2802493252@qq.com>
2019-01-09 21:39:48 +0800

    totel上部完成

--- src/views/Hotel.vue
+++ src/views/Hotel.vue
@@ -7,5 +7,15 @@
                <router-link class="right_topimg" to=""><i class="ioc right_ico"></i><p>附近酒店</p></router-link>
             </div>
         </header>
+        <div class="line-tab-box">
+            <ul>
+                <li class="allday">全日房</li>
+                <li class="clock">钟点房</li>
+            </ul>
+            <div class="line"></div>
+        </div>
+        <div class="condition-search">
+            
+        </div>
     </div>
 </template>
