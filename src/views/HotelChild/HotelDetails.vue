bdd935d867cbefbb7c19325745b6222687ebb76d
zhengyunan <2802493252@qq.com>
2019-01-21 16:51:58 +0800

    酒店详情页基本变成

--- src/views/HotelChild/HotelDetails.vue
+++ src/views/HotelChild/HotelDetails.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="hoteldetails">
-        <header>
-            
+    <div class="hoteldetails" style="background-color='red'">
+        <header>           
             <div class="hotel_img">
                <wv-swipe class="swipe_s" :height="196" :autoplay="4000" :show-indicators="false">
                     <wv-swipe-item><img src="../../assets/images/hotelImg/2.jpg" alt=""></wv-swipe-item>
@@ -17,5 +16,137 @@
                
             </nav>
         </header>
+        <div class="mh-base-info">
+            <div class="name"><h1>丽枫酒店(松原实际新城店)</h1></div>
+            <div class="pinglun">
+                <div class="pinglun_left">
+                    <span>4.7</span>
+                    <span>好极了</span>
+                </div>
+                <div class="pinglun_right">
+                     <span>
+                         距离火车站较近 好的交通 出门美食街 
+                     </span>
+                </div>
+            </div>
+        </div>
+        <div class="hotel_type">
+            <ul>
+                <li>
+                    <div class="hotel_type_node">
+                        <div class="li_left">
+                            <img src="../../assets/images/hotelImg/3.jpg" alt="">
+                        </div>
+                        <div class="li_middle">
+                            <h2>豪华间</h2>
+                            <p>28㎡</p>
+                        </div>
+                        <div class="li_right">
+                            <p>
+                              <span>￥</span>
+                              <span>345</span>
+                              <span>起</span>
+                            </p>
+                            <span class="hotel_online_booking">在线预订</span>
+                        </div>
+                    </div>
+                </li>
+               <li>
+                    <div class="hotel_type_node">
+                        <div class="li_left">
+                            <img src="../../assets/images/hotelImg/3.jpg" alt="">
+                        </div>
+                        <div class="li_middle">
+                            <h2>豪华间</h2>
+                            <p>28㎡</p>
+                        </div>
+                        <div class="li_right">
+                            <p>
+                              <span>￥</span>
+                              <span>345</span>
+                              <span>起</span>
+                            </p>
+                            <span class="hotel_online_booking">在线预订</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="hotel_type_node">
+                        <div class="li_left">
+                            <img src="../../assets/images/hotelImg/3.jpg" alt="">
+                        </div>
+                        <div class="li_middle">
+                            <h2>豪华间</h2>
+                            <p>28㎡</p>
+                        </div>
+                        <div class="li_right">
+                            <p>
+                              <span>￥</span>
+                              <span>345</span>
+                              <span>起</span>
+                            </p>
+                            <span class="hotel_online_booking">在线预订</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="hotel_type_node">
+                        <div class="li_left">
+                            <img src="../../assets/images/hotelImg/3.jpg" alt="">
+                        </div>
+                        <div class="li_middle">
+                            <h2>豪华间</h2>
+                            <p>28㎡</p>
+                        </div>
+                        <div class="li_right">
+                            <p>
+                              <span>￥</span>
+                              <span>345</span>
+                              <span>起</span>
+                            </p>
+                            <span class="hotel_online_booking">在线预订</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="hotel_type_node">
+                        <div class="li_left">
+                            <img src="../../assets/images/hotelImg/3.jpg" alt="">
+                        </div>
+                        <div class="li_middle">
+                            <h2>豪华间</h2>
+                            <p>28㎡</p>
+                        </div>
+                        <div class="li_right">
+                            <p>
+                              <span>￥</span>
+                              <span>345</span>
+                              <span>起</span>
+                            </p>
+                            <span class="hotel_online_booking">在线预订</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="hotel_type_node">
+                        <div class="li_left">
+                            <img src="../../assets/images/hotelImg/3.jpg" alt="">
+                        </div>
+                        <div class="li_middle">
+                            <h2>豪华间</h2>
+                            <p>28㎡</p>
+                        </div>
+                        <div class="li_right">
+                            <p>
+                              <span>￥</span>
+                              <span>345</span>
+                              <span>起</span>
+                            </p>
+                            <span class="hotel_online_booking">在线预订</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
